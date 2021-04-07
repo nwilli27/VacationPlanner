c4b2e02416c532a8215d238d4a976870549e6c46
Nolan Williams <ntwilliams18@gmail.com>
2021-04-07 19:08:15 -0400

    Added app DbContext base implementation & hooked it up in startup

---
+++ WilliamsVacationPlanner/Models/Database/VacationPlannerContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models
+{
+	public class VacationPlannerContext : DbContext
+	{
+		#region Methods
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+
+		}
+
+		#endregion
+	}
+}
--- WilliamsVacationPlanner/Startup.cs
+++ WilliamsVacationPlanner/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,25 +9,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WilliamsVacationPlanner.Models;
 
 namespace WilliamsVacationPlanner
 {
+	/// <summary>
+	/// Handles application setup
+	/// 
+	/// Author: Nolan Williams
+	/// Date:	4/7/2021
+	/// </summary>
 	public class Startup
 	{
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Startup"/> class.
+		/// </summary>
+		/// <param name="configuration">The configuration.</param>
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
 
+		/// <summary>
+		/// Gets the configuration.
+		/// </summary>
+		/// <value>
+		/// The configuration.
+		/// </value>
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
+		/// <summary>
+		/// Configures the services.
+		/// </summary>
+		/// <param name="services">The services.</param>
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddRazorPages();
+
+			services.AddDbContext<VacationPlannerContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("VacationPlannerContext")));
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		/// <summary>
+		/// Configures the specified application.
+		/// </summary>
+		/// <param name="app">The application.</param>
+		/// <param name="env">The env.</param>
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -36,7 +64,6 @@ namespace WilliamsVacationPlanner
 			else
 			{
 				app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
 
@@ -49,7 +76,7 @@ namespace WilliamsVacationPlanner
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapRazorPages();
+				endpoints.MapControllerRoute("default", "{controller=Trips}/{action=Index}/{id?}");
 			});
 		}
 	}
