fe057cf9af78d266cf791d8772c44fee97519c1d
Nolan Williams <ntwilliams18@gmail.com>
2021-04-07 22:43:46 -0400

    Adding Vacation query options & grid builder to assist ordering the grid of vacations

---
+++ WilliamsVacationPlanner/Models/Database/Utility/VacationQueryOptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models.Database.Utility
+{
+    /// <summary>
+    /// Holds query options for a Vacation
+    /// 
+    /// Author: Nolan Williams
+    /// Date:   4/7/2021
+    /// </summary>
+    /// <seealso cref="WilliamsSalesRep.Models.QueryOptions{WilliamsSalesRep.Models.Sale}" />
+    public class VacationQueryOptions : QueryOptions<Vacation>
+    {
+
+        /// <summary>
+        /// Appends Where & OrderBy clauses based on the given [builder].
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        public void SortFilter(VacationGridBuilder builder)
+        {
+            if (builder.IsSortByLocation)
+            {
+                OrderBy = v => v.Location.Name;
+            }
+            else if (builder.IsSortByStartDate)
+            {
+                OrderBy = v => v.StartDate;
+            }
+            else if (builder.IsSortByEndDate)
+            {
+                OrderBy = v => v.EndDate;
+            }
+            else if (builder.IsSortByAccommodation)
+            {
+                OrderBy = v => v.Accommodation.Name;
+            }
+        }
+    }
+}
---
+++ WilliamsVacationPlanner/Models/Grid/VacationGridBuilder.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models
+{
+	/// <summary>
+	/// Holds functionality to format a grid of Vacation
+	/// 
+	/// Author: Nolan Williams
+	/// Date:   4/7/2021
+	/// </summary>
+	/// <seealso cref="WilliamsSalesRep.Models.GridBuilder" />
+	public class VacationGridBuilder : GridBuilder
+	{
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="VacationGridBuilder"/> class.
+		/// </summary>
+		/// <param name="sess">The sess.</param>
+		public VacationGridBuilder(ISession sess) : base(sess) { }
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is sort by location.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is sort by location; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsSortByLocation => routes.SortField.EqualsNoCase(nameof(Vacation.Location.Name));
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is sort by start date.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is sort by start date; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsSortByStartDate => routes.SortField.EqualsNoCase(nameof(Vacation.StartDate));
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is sort by end date.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is sort by end date; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsSortByEndDate => routes.SortField.EqualsNoCase(nameof(Vacation.EndDate));
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is sort by accommodation.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is sort by accommodation; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsSortByAccommodation => routes.SortField.EqualsNoCase(nameof(Vacation.Accommodation.Name));
+
+		/// <summary>
+		/// Gets a value indicating whether this instance is sort by activities.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is sort by activities; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsSortByActivities => routes.SortField.EqualsNoCase(nameof(Vacation.Activities));
+
+	}
+}
