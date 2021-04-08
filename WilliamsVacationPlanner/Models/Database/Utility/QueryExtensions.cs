1796ba8a11756851a1dd54f1cf6edcf73c7699cd
Nolan Williams <ntwilliams18@gmail.com>
2021-04-07 22:21:52 -0400

    Added classes to assist functionality with querying with Entity

---
+++ WilliamsVacationPlanner/Models/Database/Utility/QueryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models
+{
+	/// <summary>
+	/// Holds extension functionality for an IQueryable obj
+	/// 
+	/// Author: Nolan Williams
+	/// Date:   4/7/2021
+	/// </summary>
+	public static class QueryExtensions
+	{
+
+		/// <summary>
+		/// Returns the items based on the given [pagenumber] and [pagesize].
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="items">The items.</param>
+		/// <param name="pagenumber">The pagenumber.</param>
+		/// <param name="pagesize">The pagesize.</param>
+		/// <returns>Set of items split based on the pagenumber and pagesize</returns>
+		public static IQueryable<T> PageBy<T>(this IQueryable<T> items,
+			int pagenumber, int pagesize)
+		{
+			return items
+				.Skip((pagenumber - 1) * pagesize)
+				.Take(pagesize);
+		}
+	}
+}
---
+++ WilliamsVacationPlanner/Models/Database/Utility/QueryOptions.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models
+{
+	/// <summary>
+	/// Holds functionality for a Query
+	/// 
+	/// Author: Nolan Williams
+	/// Date:   4/7/2021
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public class QueryOptions<T>
+	{
+
+		/// <summary>
+		/// Gets or sets the order by.
+		/// </summary>
+		/// <value>
+		/// The order by.
+		/// </value>
+		public Expression<Func<T, Object>> OrderBy { get; set; }
+
+		/// <summary>
+		/// Gets or sets the order by direction.
+		/// </summary>
+		/// <value>
+		/// The order by direction.
+		/// </value>
+		public string OrderByDirection { get; set; } = "asc";
+
+		/// <summary>
+		/// Gets or sets the page number.
+		/// </summary>
+		/// <value>
+		/// The page number.
+		/// </value>
+		public int PageNumber { get; set; }
+
+		/// <summary>
+		/// Gets or sets the size of the page.
+		/// </summary>
+		/// <value>
+		/// The size of the page.
+		/// </value>
+		public int PageSize { get; set; }
+
+		/// <summary>
+		/// Gets or sets the where clauses.
+		/// </summary>
+		/// <value>
+		/// The where clauses.
+		/// </value>
+		public WhereClauses<T> WhereClauses { get; set; }
+
+		/// <summary>
+		/// Sets the where.
+		/// </summary>
+		/// <value>
+		/// The where.
+		/// </value>
+		public Expression<Func<T, bool>> Where
+		{
+			set
+			{
+				if (WhereClauses == null)
+				{
+					WhereClauses = new WhereClauses<T>();
+				}
+				WhereClauses.Add(value);
+			}
+		}
+
+		private string[] includes;
+
+		/// <summary>
+		/// Sets the includes.
+		/// </summary>
+		/// <value>
+		/// The includes.
+		/// </value>
+		public string Includes
+		{
+			set => includes = value.Replace(" ", "").Split(',');
+		}
+
+		/// <summary>
+		/// Gets the includes.
+		/// </summary>
+		/// <returns></returns>
+		public string[] GetIncludes() => includes ?? new string[0];
+
+		/// <summary>
+		/// Gets a value indicating whether this instance has where.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance has where; otherwise, <c>false</c>.
+		/// </value>
+		public bool HasWhere => WhereClauses != null;
+
+		/// <summary>
+		/// Gets a value indicating whether this instance has order by.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance has order by; otherwise, <c>false</c>.
+		/// </value>
+		public bool HasOrderBy => OrderBy != null;
+
+		/// <summary>
+		/// Gets a value indicating whether this instance has paging.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance has paging; otherwise, <c>false</c>.
+		/// </value>
+		public bool HasPaging => PageNumber > 0 && PageSize > 0;
+	}
+
+	/// <summary>
+	/// Extends functionality from a list of generic expressions
+	/// 
+	/// Author: Nolan Williams
+	/// Date:	4/3/2021
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <seealso cref="System.Collections.Generic.List{System.Linq.Expressions.Expression{System.Func{T, System.Boolean}}}" />
+	public class WhereClauses<T> : List<Expression<Func<T, bool>>> { }
+}
