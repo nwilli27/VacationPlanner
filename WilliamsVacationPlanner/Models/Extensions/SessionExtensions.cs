94d63129fe507f35d555ce3310cbe7d4a84b21ac
Nolan Williams <ntwilliams18@gmail.com>
2021-04-07 22:26:44 -0400

    Added extension methods to assist with ISession & strings

---
+++ WilliamsVacationPlanner/Models/Extensions/SessionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models
+{
+	/// <summary>
+	/// Holds extension methods for an ISession obj
+	/// 
+	/// Author: Nolan Williams
+	/// Date:   4/7/2021
+	/// </summary>
+	public static class SessionExtensions
+	{
+
+		/// <summary>
+		/// Allows you to set the generic [value] with the given [key] in the [session] obj.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="session">The session.</param>
+		/// <param name="key">The key.</param>
+		/// <param name="value">The value.</param>
+		public static void SetObject<T>(this ISession session, string key, T value) =>
+			session.SetString(key, JsonConvert.SerializeObject(value));
+
+		/// <summary>
+		/// Returns the generic obj from the session with the given [key].
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="session">The session.</param>
+		/// <param name="key">The key.</param>
+		/// <returns>Object from session with given [key].</returns>
+		public static T GetObject<T>(this ISession session, string key)
+		{
+			var value = session.GetString(key);
+			return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+		}
+	}
+}
---
+++ WilliamsVacationPlanner/Models/Extensions/StringExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models.Extensions
+{
+	/// <summary>
+	/// Holds extension methods for a string
+	/// 
+	/// Author: Nolan Williams
+	/// Date:   4/7/2021
+	/// </summary>
+	public static class StringExtensions
+	{
+
+		/// <summary>
+		/// Creates a formatted slug.
+		/// </summary>
+		/// <param name="s">The s.</param>
+		/// <returns>A slug formatted from the string</returns>
+		public static string Slug(this string s)
+		{
+			var sb = new StringBuilder();
+			foreach (char c in s)
+			{
+				if (!char.IsPunctuation(c) || c == '-')
+				{
+					sb.Append(c);
+				}
+			}
+			return sb.ToString().Replace(' ', '-').ToLower();
+		}
+
+		/// <summary>
+		/// Returns true if strings are equal after being converted to lower case.
+		/// </summary>
+		/// <param name="s">The s.</param>
+		/// <param name="tocompare">The tocompare.</param>
+		/// <returns>True if equal; otherwise false</returns>
+		public static bool EqualsNoCase(this string s, string tocompare) =>
+			s?.ToLower() == tocompare?.ToLower();
+
+		/// <summary>
+		/// Converts the string representation of a number to an integer.
+		/// </summary>
+		/// <param name="s">The s.</param>
+		/// <returns></returns>
+		public static int ToInt(this string s)
+		{
+			int.TryParse(s, out int id);
+			return id;
+		}
+
+		/// <summary>
+		/// Capitalizes the specified string.
+		/// </summary>
+		/// <param name="s">The s.</param>
+		/// <returns></returns>
+		public static string Capitalize(this string s) =>
+			s?.Substring(0, 1)?.ToUpper() + s?.Substring(1);
+	}
+}
