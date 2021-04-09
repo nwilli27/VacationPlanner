19a707bde241e5816bfb1c2b196a412f1714963b
Nolan Williams <ntwilliams18@gmail.com>
2021-04-08 22:44:58 -0400

    Added Manage view, ViewModel, & Controller needed for all delete/add options

---
+++ WilliamsVacationPlanner/Controllers/ManageController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WilliamsVacationPlanner.Models;
+
+namespace WilliamsVacationPlanner.Controllers
+{
+	/// <summary>
+	/// Holds actions for the Manage views
+	/// 
+	/// Author: Nolan Williams
+	/// Date:	4/8/2021
+	/// </summary>
+	/// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
+	public class ManageController : Controller
+	{
+		private IVacationPlannerDataAccessor data { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ManageController"/> class.
+		/// </summary>
+		/// <param name="dataAccessor">The data accessor.</param>
+		/// <param name="httpCtx">The HTTP CTX.</param>
+		public ManageController(IVacationPlannerDataAccessor dataAccessor)
+		{
+			this.data = dataAccessor;
+		}
+
+		/// <summary>
+		/// Redirects to the Manage view.
+		/// </summary>
+		/// <returns>Redirect to manage view.</returns>
+		public IActionResult Index() => RedirectToAction("Manage");
+
+		/// <summary>
+		/// Manages this instance.
+		/// </summary>
+		/// <returns></returns>
+		public IActionResult Manage()
+		{
+			var viewModel = new ManagePlannerViewModel()
+			{
+				LocationToAdd = new Location(),
+				AccommodationToAdd = new Accommodation(),
+				ActivityToAdd = new Activity(),
+				LocationToDelete = new Location(),
+				AccommodationToDelete = new Accommodation(),
+				ActivityToDelete = new Activity(),
+				Locations = data.Locations.List(new QueryOptions<Location>() { OrderBy = l => l.Name }),
+				Accomodations = data.Accommodations.List(new QueryOptions<Accommodation>() { OrderBy = a => a.Name }),
+				Activities = data.Activities.List(new QueryOptions<Activity>() { OrderBy = a => a.Name })
+			};
+
+			return View(viewModel);
+		}
+	}
+}
---
+++ WilliamsVacationPlanner/Models/ViewModels/ManagePlannerViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WilliamsVacationPlanner.Models
+{
+	/// <summary>
+	/// Holds data needed for the Manage planner view
+	/// 
+	/// Author: Nolan Williams
+	/// Date:	4/8/2021
+	/// </summary>
+	public class ManagePlannerViewModel
+	{
+
+		/// <summary>
+		/// Gets or sets the location to add.
+		/// </summary>
+		/// <value>
+		/// The location to add.
+		/// </value>
+		public Location LocationToAdd { get; set; }
+
+		/// <summary>
+		/// Gets or sets the accommodation to add.
+		/// </summary>
+		/// <value>
+		/// The accommodation to add.
+		/// </value>
+		public Accommodation AccommodationToAdd { get; set; }
+
+		/// <summary>
+		/// Gets or sets the activity to add.
+		/// </summary>
+		/// <value>
+		/// The activity to add.
+		/// </value>
+		public Activity ActivityToAdd { get; set; }
+
+		/// <summary>
+		/// Gets or sets the location to delete.
+		/// </summary>
+		/// <value>
+		/// The location to delete.
+		/// </value>
+		public Location LocationToDelete { get; set; }
+
+		/// <summary>
+		/// Gets or sets the accommodation to delete.
+		/// </summary>
+		/// <value>
+		/// The accommodation to delete.
+		/// </value>
+		public Accommodation AccommodationToDelete { get; set; }
+
+		/// <summary>
+		/// Gets or sets the activity to delete.
+		/// </summary>
+		/// <value>
+		/// The activity to delete.
+		/// </value>
+		public Activity ActivityToDelete { get; set; }
+
+		/// <summary>
+		/// Gets or sets the locations.
+		/// </summary>
+		/// <value>
+		/// The locations.
+		/// </value>
+		public IEnumerable<Location> Locations { get; set; }
+
+		/// <summary>
+		/// Gets or sets the accomodations.
+		/// </summary>
+		/// <value>
+		/// The accomodations.
+		/// </value>
+		public IEnumerable<Accommodation> Accomodations { get; set; }
+
+		/// <summary>
+		/// Gets or sets the activities.
+		/// </summary>
+		/// <value>
+		/// The activities.
+		/// </value>
+		public IEnumerable<Activity> Activities { get; set; }
+	}
+}
